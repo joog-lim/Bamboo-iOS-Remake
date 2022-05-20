1ff954cb71a5d7797a268b558ebb5e6b69d3986d
JiHoonAHN <ahnjh1028@naver.com>
2022-05-20 09:27:26 +0900

    [CREATE] Dropdown Extension 적용

--- Platform/Sources/BamBooSetting/DropDown/BamBooDropDown.swift
+++ Platform/Sources/BamBooSetting/DropDown/BamBooDropDown.swift
@@ -2,13 +2,13 @@ import DropDown
 import UIKit
 
 public extension DropDown{
-    convenience init(dataSource : [String], font : UIFont = .bamboo(size: 12, family: .Bold), cellHeight : CGFloat = 29, anchorView : UIView?){
+    convenience init(dataSource : [String], font : UIFont = .bamboo(size: 12, family: .Bold), cellHeight : CGFloat = 15, anchorView : UIView?){
         self.init()
         self.anchorView = anchorView
         self.dataSource = dataSource
         self.textFont = font
         self.cellHeight = cellHeight
-        self.cornerRadius = 10
+        self.cornerRadius = 5
         self.shadowOpacity = 0.25
         self.backgroundColor = .white
         self.selectionBackgroundColor = .clear
--- bamboo-iOS/Sources/Scene/Modal/WritingBulletinBoardModal/WritingBulletinBoardModalViewController.swift
+++ bamboo-iOS/Sources/Scene/Modal/WritingBulletinBoardModal/WritingBulletinBoardModalViewController.swift
@@ -17,7 +17,7 @@ final class WritingBulletinBoardModalViewController: BaseVC<WritingBulletinBoard
     private let explanationLabel = UILabel(text: "올리고 싶은 글을 입력해주세요!", font: .bamboo(size: 12, family: .Regular))
     private let titleTf = AlertTextField(placeholder: "제목을 입력해주세요!")
     private let tagChooseBtn = UIButton(text: "태그선택", backgroundColor: .bambooGreen,cornerRadius:  5, font: .bamboo(size: 10, family: .Regular))
-    private lazy var dropdown = DropDown(anchorView: tagChooseBtn, dataSource: ["유머","공부","일상","학교","취업","관계","기타"])
+    private lazy var dropdown = DropDown(dataSource: ["유머","공부","일상","학교","취업","관계","기타"],anchorView: tagChooseBtn)
     private let contentTv = AlertTextView(placeholder: "내용을 입력하세요.")
     private let passwordTitle = UILabel(text: "학교 와이파이 번호", font: .bamboo(size: 12, family: .Regular))
     private let passwordTf = AlertTextField(placeholder: "답변을 입력하세요.")
